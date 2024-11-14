aab517e6abddab606a948f7715c142a97eab46ac
Mantofka <mantasp147@gmail.com>
2024-11-14 23:42:48 +0200

    Added advanced and simple movement test

--- Tests/Strategy/AdvancedMovementTests.cs
+++ Tests/Strategy/AdvancedMovementTests.cs
@@ -6,7 +6,7 @@ namespace Tests.Strategy;
 public class AdvancedMovementTests
 {
     [Fact]
-    public void AdvancedMovement_ShouldMoveEnemy_TowardsClosestPlayer()
+    public void AdvancedMovement_ShouldMoveEnemy_TowardsPlayer()
     {
         var enemy = new MobileExplosiveEnemy(5, 5, 100, 1);
         enemy.SetMovementBehaviour(new AdvancedMovement());
@@ -28,6 +28,43 @@ public class AdvancedMovementTests
     }
 
     [Fact]
+    public void AdvancedMovement_ShouldMoveEnemy_TowardsClosestPlayer()
+    {
+        var enemy = new MobileExplosiveEnemy(5, 5, 100, 1);
+        enemy.SetMovementBehaviour(new AdvancedMovement());
+
+        var player1 = new Player()
+        {
+            Id = 10,
+            X = 10,
+            Y = 10,
+            Image = "PlayerUp",
+            Health = 40,
+            Speed = 10,
+        };
+        var player2 = new Player()
+        {
+            Id = 3,
+            X = 3,
+            Y = 3,
+            Image = "PlayerUp",
+            Health = 40,
+            Speed = 10,
+        };
+
+        var players = new List<Player>
+    {
+        player1,
+        player2
+    };
+
+        enemy.PerformMovement(players);
+
+        Assert.InRange(enemy.X, 4, 5);
+        Assert.InRange(enemy.Y, 4, 5);
+    }
+
+    [Fact]
     public void AdvancedMovement_ShouldNotMoveEnemy_WhenNoPlayersArePresent()
     {
         var enemy = new MobileExplosiveEnemy(5, 5, 100, 1);
@@ -37,6 +74,20 @@ public class AdvancedMovementTests
         
         enemy.PerformMovement(new List<Player>());
         
+        Assert.Equal(initialX, enemy.X);
+        Assert.Equal(initialY, enemy.Y);
+    }
+
+    [Fact]
+    public void AdvancedMovement_ShouldNotMove_WhenNoPlayers()
+    {
+        var enemy = new MobileExplosiveEnemy(5, 5, 100, 1);
+        enemy.SetMovementBehaviour(new AdvancedMovement());
+        var initialX = enemy.X;
+        var initialY = enemy.Y;
+
+        enemy.PerformMovement(new List<Player>());
+
         Assert.Equal(initialX, enemy.X);
         Assert.Equal(initialY, enemy.Y);
     }
--- Tests/Strategy/SimpleMovementTests.cs
+++ Tests/Strategy/SimpleMovementTests.cs
@@ -8,7 +8,7 @@ public class SimpleMovementTests
     public void SimpleMovement_ShouldMoveEnemy_RandomlyWithinBounds()
     {
         var enemy = new MobileExplosiveEnemy(5, 5, 100, 1);
-        enemy.SetMovementBehaviour(new AdvancedMovement());
+        enemy.SetMovementBehaviour(new SimpleMovement());
         var initialX = enemy.X;
         var initialY = enemy.Y;
         
@@ -16,5 +16,33 @@ public class SimpleMovementTests
         
         Assert.InRange(enemy.X, initialX - 1, initialX + 1);
         Assert.InRange(enemy.Y, initialY - 1, initialY + 1);
+    }
+
+    [Fact]
+    public void MobileShootingEnemy_ShouldMoveRandomlyWithinBounds_WithSimpleMovement()
+    {
+        var enemy = new MobileShootingEnemy(5, 5, 100, 2);
+        enemy.SetMovementBehaviour(new SimpleMovement());
+        var initialX = enemy.X;
+        var initialY = enemy.Y;
+
+        enemy.PerformMovement(new List<Player>());
+
+        Assert.InRange(enemy.X, initialX - 1, initialX + 1);
+        Assert.InRange(enemy.Y, initialY - 1, initialY + 1);
+    }
+
+    [Fact]
+    public void Enemy_ShouldSetMovementBehaviour_Correctly()
+    {
+        var enemy = new MobileExplosiveEnemy(5, 5, 100, 1);
+        var simpleMovement = new SimpleMovement();
+        var advancedMovement = new AdvancedMovement();
+
+        enemy.SetMovementBehaviour(simpleMovement);
+        Assert.IsType<SimpleMovement>(enemy.MovementBehaviour);
+
+        enemy.SetMovementBehaviour(advancedMovement);
+        Assert.IsType<AdvancedMovement>(enemy.MovementBehaviour);
     }
 }
