38fe7a093e03dbb3abe6b08e6f8cf1370317e65e
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2024-12-19 20:33:53 +0200

    - add memes

--- Client/ClientUpdateHandler.cs
+++ Client/ClientUpdateHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Client.Composite;
 using Client.HubClients;
+using Client.Memento;
 using Client.Observer;
 using System.Windows.Forms;
 
@@ -8,83 +9,88 @@ namespace Client;
 // Facade
 public class ClientUpdateHandler
 {
-    private readonly INetworkHandler _networkHandler;
-    private readonly ContainerControl _containerControl;
+	private readonly INetworkHandler _networkHandler;
+	private readonly ContainerControl _containerControl;
 
-    private readonly InputPublisher _inputPublisher = new();
-    private readonly MovementHandler _movementHandler = new();
-    private readonly CrosshairRenderer _crosshairRenderer = new();
-    private readonly PauseHandler _pauseHandler = new();
+	private readonly InputPublisher _inputPublisher = new();
+	private readonly MovementHandler _movementHandler = new();
+	private readonly CrosshairRenderer _crosshairRenderer = new();
+	private readonly PauseHandler _pauseHandler = new();
 
-    private readonly MenuStrip _menu;
-    private bool _menuIsActive = false;
+	private readonly GameTitle _gameTitle = new();
+	private readonly GameTitleHandler _gameTitleHandler;
 
-    private readonly List<object> _components = [];
+	private readonly MenuStrip _menu;
+	private bool _menuIsActive = false;
 
-    public ClientUpdateHandler(ContainerControl containerControl, INetworkHandler networkHandler)
-    {
-        _containerControl = containerControl;
-        _networkHandler = networkHandler;
+	private readonly List<object> _components = [];
 
-        _inputPublisher.AddObserver(_movementHandler);
-        _inputPublisher.AddObserver(_crosshairRenderer);
-        _inputPublisher.AddObserver(_pauseHandler);
+	public ClientUpdateHandler(ContainerControl containerControl, INetworkHandler networkHandler)
+	{
+		_containerControl = containerControl;
+		_networkHandler = networkHandler;
 
-        _crosshairRenderer.Init(_containerControl.Controls);
-        _pauseHandler.Init(_containerControl.Controls);
+		_inputPublisher.AddObserver(_movementHandler);
+		_inputPublisher.AddObserver(_crosshairRenderer);
+		_inputPublisher.AddObserver(_pauseHandler);
 
-        _components.Add(_inputPublisher);
-        _components.Add(_movementHandler);
-        _components.Add(_crosshairRenderer);
-        _components.Add(_pauseHandler);
+		_crosshairRenderer.Init(_containerControl.Controls);
+		_pauseHandler.Init(_containerControl.Controls);
 
-        _menu = CreateMenu();
-    }
+		_components.Add(_inputPublisher);
+		_components.Add(_movementHandler);
+		_components.Add(_crosshairRenderer);
+		_components.Add(_pauseHandler);
 
-    public void OnKeyDown(object? sender, KeyEventArgs e) =>
-        HandleInput(new InputEvent()
-        {
-            KeyboardEvent = new(e, KeyEventType.KeyDown)
-        });
+		_gameTitleHandler = new GameTitleHandler(_gameTitle);
 
-    public void OnKeyUp(object? sender, KeyEventArgs e) =>
-        HandleInput(new InputEvent()
-        {
-            KeyboardEvent = new(e, KeyEventType.KeyUp)
-        });
+		_menu = CreateMenu();
+	}
 
-    public void OnWindowsMessage(ref Message message)
-    {
-        const int WM_MOUSEMOVE = 0x0200;
+	public void OnKeyDown(object? sender, KeyEventArgs e) =>
+		HandleInput(new InputEvent()
+		{
+			KeyboardEvent = new(e, KeyEventType.KeyDown)
+		});
 
-        if (message.Msg == WM_MOUSEMOVE)
-        {
-            var clientCoords = _containerControl.PointToClient(Cursor.Position);
+	public void OnKeyUp(object? sender, KeyEventArgs e) =>
+		HandleInput(new InputEvent()
+		{
+			KeyboardEvent = new(e, KeyEventType.KeyUp)
+		});
 
-            HandleInput(new InputEvent()
-            {
-                MouseEvent = new(clientCoords, MouseEventType.Move)
-            });
-        }
-    }
+	public void OnWindowsMessage(ref Message message)
+	{
+		const int WM_MOUSEMOVE = 0x0200;
 
-    /// <summary> Public only for testing. </summary>
-    public void HandleInput(InputEvent inputEvent)
-    {
-        _inputPublisher.AddInputEvent(inputEvent);
-        _inputPublisher.NotifyObservers();
-    }
+		if (message.Msg == WM_MOUSEMOVE)
+		{
+			var clientCoords = _containerControl.PointToClient(Cursor.Position);
 
-    public async Task<bool> UpdateAsync()
-    {
-        if (_pauseHandler.IsPaused)
-        {
-            _crosshairRenderer.Update();
+			HandleInput(new InputEvent()
+			{
+				MouseEvent = new(clientCoords, MouseEventType.Move)
+			});
+		}
+	}
 
-            if(!_menuIsActive)
-            {
+	/// <summary> Public only for testing. </summary>
+	public void HandleInput(InputEvent inputEvent)
+	{
+		_inputPublisher.AddInputEvent(inputEvent);
+		_inputPublisher.NotifyObservers();
+	}
+
+	public async Task<bool> UpdateAsync()
+	{
+		if (_pauseHandler.IsPaused)
+		{
+			_crosshairRenderer.Update();
+
+			if (!_menuIsActive)
+			{
 				_menuIsActive = true;
-                _containerControl.Controls.Add(_menu);
+				_containerControl.Controls.Add(_menu);
 
 				if (_containerControl is Form form)
 				{
@@ -94,11 +100,11 @@ public class ClientUpdateHandler
 				_menu.BringToFront();
 			}
 
-            return false;
-        }
-        else if(_menuIsActive)
-        {
-            _menuIsActive = false;
+			return false;
+		}
+		else if (_menuIsActive)
+		{
+			_menuIsActive = false;
 			_containerControl.Controls.Remove(_menu);
 
 			if (_containerControl is Form form)
@@ -107,46 +113,55 @@ public class ClientUpdateHandler
 			}
 		}
 
-        await _movementHandler.SendAsync(_networkHandler);
+		await _movementHandler.SendAsync(_networkHandler);
 
-        foreach (var updateable in _components.OfType<IUpdateable>())
-        {
-            updateable.Update();
-        }
+		foreach (var updateable in _components.OfType<IUpdateable>())
+		{
+			updateable.Update();
+		}
 
-        return true;
-    }
-    // Add method to handle teleportation commands
-    public async Task HandleTeleportationAsync(int x, int y)
-    {
-        // Send teleport coordinates to the server via the network handler
-        await _networkHandler.SendTeleportAsync(x, y, Globals.PersonalID);
-        Console.WriteLine($"Teleporting player to ({x}, {y})...");
-    }
+		return true;
+	}
+	// Add method to handle teleportation commands
+	public async Task HandleTeleportationAsync(int x, int y)
+	{
+		// Send teleport coordinates to the server via the network handler
+		await _networkHandler.SendTeleportAsync(x, y, Globals.PersonalID);
+		Console.WriteLine($"Teleporting player to ({x}, {y})...");
+	}
 
-    public async Task HandleEnemyDamageAsync(int id)
-    {
-	    await _networkHandler.DamageEnemy(id);
-    }
+	public async Task HandleEnemyDamageAsync(int id)
+	{
+		await _networkHandler.DamageEnemy(id);
+	}
 
-    public async Task HandleBulletFired(int id)
-    {
-        await _networkHandler.BulletFired(id);
-    }
+	public async Task HandleBulletFired(int id)
+	{
+		await _networkHandler.BulletFired(id);
+	}
 
-    private static MenuStrip CreateMenu()
-    {
+	private MenuStrip CreateMenu()
+	{
 		var menuStrip = new MenuStrip();
 
 		// Build the menu structure
 		var fileMenu = new MenuComposite("File");
 		fileMenu.Add(new MenuItemLeaf("New", (s, e) => MessageBox.Show("New clicked!")));
 		fileMenu.Add(new MenuItemLeaf("Open", (s, e) => MessageBox.Show("Open clicked!")));
-		fileMenu.Add(new MenuItemLeaf("Save", (s, e) => MessageBox.Show("Save clicked!")));
+		fileMenu.Add(new MenuItemLeaf("Save", (s, e) => 
+		{
+			_gameTitleHandler.Backup();
+			MessageBox.Show("Current title saved. Undo if need be ¯\\_(ツ)_/¯");
+		}));
 		fileMenu.Add(new MenuItemLeaf("Exit", (s, e) => Application.Exit()));
 
 		var editMenu = new MenuComposite("Edit");
-		editMenu.Add(new MenuItemLeaf("Undo", (s, e) => MessageBox.Show("Undo clicked!")));
+		editMenu.Add(new MenuItemLeaf("Change Title", (s, e) =>
+		{
+			var newTitle = Microsoft.VisualBasic.Interaction.InputBox("Prompt", "Title", "Default", 0, 0);
+			_gameTitle.SetTitle(newTitle);
+		}));
+		editMenu.Add(new MenuItemLeaf("Undo", (s, e) => _gameTitleHandler.Undo()));
 		editMenu.Add(new MenuItemLeaf("Redo", (s, e) => MessageBox.Show("Redo clicked!")));
 
 		var mainMenu = new MenuComposite("Main Menu");
@@ -156,6 +171,6 @@ public class ClientUpdateHandler
 		// Add the menu to the MenuStrip
 		menuStrip.Items.Add(mainMenu.CreateMenuItem());
 
-        return menuStrip;
+		return menuStrip;
 	}
 }
---
+++ Client/Memento/GameTitle.cs
@@ -0,0 +1,67 @@
+﻿namespace Client.Memento;
+
+public class GameTitle
+{
+	public string Title { get; private set; } = "Form1";
+
+	public GameTitleMemento Save()
+	{
+		return new GameTitleMemento(Title);
+	}
+
+	public void Restore(GameTitleMemento meme)
+	{
+		SetTitle(meme.GetState());
+	}
+
+	public void SetTitle(string title)
+	{
+		Title = title;
+		Globals.Form.Text = Title;
+	}
+}
+
+public class GameTitleMemento
+{
+	private readonly string _title;
+
+	public GameTitleMemento(string title)
+	{
+		_title = title;
+	}
+
+	public string GetState()
+	{
+		return _title;
+	}
+}
+
+public class GameTitleHandler
+{
+	private readonly List<GameTitleMemento> _memes = [];
+	private readonly GameTitle _gameTitle;
+
+	public GameTitleHandler(GameTitle gameTitle)
+	{
+		_gameTitle = gameTitle;
+	}
+
+	public void Backup()
+	{
+		var meme = _gameTitle.Save();
+		_memes.Add(meme);
+	}
+
+	public void Undo()
+	{
+		if (_memes.Count == 0)
+		{
+			return;
+		}
+
+		var meme = _memes.Last();
+		_memes.Remove(meme);
+
+		_gameTitle.Restore(meme);
+	}
+}
