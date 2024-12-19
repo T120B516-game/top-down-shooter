14145b1f87be9d79db11dea6bd49b15cfcb0748f
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2024-12-19 21:11:36 +0200

    - mediator

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -1,4 +1,5 @@
 using Client.HubClients;
+using Client.Mediator;
 using Microsoft.AspNetCore.SignalR.Client;
 using Newtonsoft.Json;
 using Shared;
@@ -7,164 +8,153 @@ namespace Client;
 
 public partial class Form1 : Form, IMessageFilter
 {
-    public bool IsReady { get; set; }
+	public bool IsReady { get; set; }
 
-    public ClientUpdateHandler UpdateHandler { get; private set; }
-    private IHubClient? _mainHubClient;
-    private List<Obstacle> _obstacles = [];
-    private ShootingHandler _shootingHandler = new ShootingHandler();
+	public ClientUpdateHandler UpdateHandler { get; private set; }
+	private IHubClient? _mainHubClient;
+	private List<Obstacle> _obstacles = [];
+	private ShootingHandler _shootingHandler = new ShootingHandler();
 
-    public Form1()
-    {
-        Globals.Form = this;
+	private AssetManager _assetManager = new();
+	private NetworkComponent _networkComponent;
+	private GameMediator _mediator;
 
-        InitializeComponent();
-        Application.AddMessageFilter(this);
-        this.Paint += new PaintEventHandler(OnPaint);
-        this.KeyDown += KeyIsDown;
+	public Form1()
+	{
+		Globals.Form = this;
 
-        InitializeWeapon();
-    }
+		InitializeComponent();
+		Application.AddMessageFilter(this);
+		this.Paint += new PaintEventHandler(OnPaint);
+		this.KeyDown += KeyIsDown;
 
-    public async void Form1_Load(object sender, EventArgs e)
-    {
-        _mainHubClient = await MainHubClient.GetClientAsync();
+		InitializeWeapon();
+	}
 
-        UpdateHandler = new(this, new NetworkHandler(_mainHubClient.Connection));
-        KeyDown += UpdateHandler.OnKeyDown;
-        KeyUp += UpdateHandler.OnKeyUp;
+	public async void Form1_Load(object sender, EventArgs e)
+	{
+		_mainHubClient = await MainHubClient.GetClientAsync();
 
-        _mainHubClient.Connection.On<string?, string?>("ReceiveGameUpdate", (playerUpdateResponseJson, enemiesUpdateResponseJson) =>
-        {
-            Invoke(() => OnReceiveGameUpdate(playerUpdateResponseJson, enemiesUpdateResponseJson));
-        });
-        _mainHubClient.Connection.On<int>("ReceivePersonalId", id =>
-        {
-            Globals.PersonalID = id;
-            Invoke(() => label1.Text = $"Personal id: {id}");
-        });
-        _mainHubClient.Connection.On<string>("ReceiveObstaclesUpdate", obstaclesJson =>
-        {
-            _obstacles = DeserializerObstacles.DeserializeObstacles(obstaclesJson);
-            Invalidate();
-        });
-        _mainHubClient.Connection.On<int>("RemoveEnemy", enemyId =>
-        {
-            Invoke(() => RemoveEnemy(enemyId));
-        });
-        _mainHubClient.Connection.On<int, int, int, string>("SpawnBullet", (playerId, x, y, direction) =>
-        {
-            Invoke(() => SpawnBullet(playerId, x, y, direction));
-        });
+		_assetManager = new AssetManager();
+		_networkComponent = new NetworkComponent();
+		_mediator = new GameMediator(_assetManager, _networkComponent);
 
-        await _mainHubClient.Connection.SendAsync("CreatePlayer");
+		UpdateHandler = new(this, new NetworkHandler(_mainHubClient.Connection));
+		KeyDown += UpdateHandler.OnKeyDown;
+		KeyUp += UpdateHandler.OnKeyUp;
 
-        IsReady = true;
-    }
-
-    private void OnPaint(object sender, PaintEventArgs e)
-    {
-        Graphics g = e.Graphics;
-
-        foreach (var obstacle in _obstacles)
-        {
-            obstacle.Draw(g, Globals.ColliderMap);
-        }
-    }
-
-    private void InitializeWeapon()
-    {
-        Weapon initialWeapon = new Pistol();
-        _shootingHandler.SetWeapon(initialWeapon);
-    }
-
-    private void OnReceiveGameUpdate(string PlayersJson, string EnemiesJson)
-    {
-        var players = JsonConvert.DeserializeObject<List<Player>>(PlayersJson);
-
-        Player? thisPlayer = players.Where(p => p.Id == Globals.PersonalID).FirstOrDefault();
-        if (thisPlayer != null)
-            Globals.ThisPlayer.UpdatePlayer(thisPlayer);
-
-        players.ForEach(Render);
-
-        var enemies = DeserializeEnemies.DeserializeEnemy(EnemiesJson);
-        enemies.ForEach(Render);
-    }
-
-    private void Render(IRenderable renderable)
-    {
-        PictureBox? existingPicture = null;
-        foreach (var picture in this.Controls.OfType<PictureBox>())
-        {
-            if (picture.Tag is int numericTag && numericTag == renderable.Id)
-            {
-                existingPicture = picture;
-            }
-        }
-
-        if (existingPicture == null)
-        {
-            existingPicture = new();
-
-            Update(existingPicture, renderable);
-            this.Controls.Add(existingPicture);
-        }
-        else
-        {
-            Update(existingPicture, renderable);
-        }
-    }
-
-    private void Update(PictureBox picture, IRenderable renderable)
-    {
-        picture.Tag = renderable.Id;
-        picture.Left = renderable.X;
-        picture.Top = renderable.Y;
-        picture.SizeMode = PictureBoxSizeMode.AutoSize;
-        picture.Image = (Bitmap)Sprites.ResourceManager.GetObject(renderable.Image);
-    }
-
-    public bool PreFilterMessage(ref Message m)
-    {
-        UpdateHandler?.OnWindowsMessage(ref m);
-        return false;
-    }
-
-    private void label1_Click(object sender, EventArgs e) { }
-
-    private void RemoveEnemy(int enemyId) 
-    {
-		foreach (var picture in this.Controls.OfType<PictureBox>())
+		_mainHubClient.Connection.On<string?, string?>("ReceiveGameUpdate", (playerUpdateResponseJson, enemiesUpdateResponseJson) =>
 		{
-			if (picture.Tag is int numericTag && numericTag == enemyId)
-			{
-				picture.Dispose();
-			}
+			Invoke(() => OnReceiveGameUpdate(playerUpdateResponseJson, enemiesUpdateResponseJson));
+		});
+		_mainHubClient.Connection.On<int>("ReceivePersonalId", id =>
+		{
+			Globals.PersonalID = id;
+			Invoke(() => label1.Text = $"Personal id: {id}");
+		});
+		_mainHubClient.Connection.On<string>("ReceiveObstaclesUpdate", obstaclesJson =>
+		{
+			_obstacles = DeserializerObstacles.DeserializeObstacles(obstaclesJson);
+			Invalidate();
+		});
+		_mainHubClient.Connection.On<int>("RemoveEnemy", enemyId =>
+		{
+			_networkComponent.OnRemoveEnemy(enemyId);
+		});
+		_mainHubClient.Connection.On<int, int, int, string>("SpawnBullet", (playerId, x, y, direction) =>
+		{
+			_networkComponent.OnSpawnBullet(playerId, x, y, direction);
+		});
+
+		await _mainHubClient.Connection.SendAsync("CreatePlayer");
+
+		IsReady = true;
+	}
+
+	private void OnPaint(object sender, PaintEventArgs e)
+	{
+		Graphics g = e.Graphics;
+
+		foreach (var obstacle in _obstacles)
+		{
+			obstacle.Draw(g, Globals.ColliderMap);
 		}
 	}
 
-    private void SpawnBullet(int playerId, int x, int y, string direction)
-    {
-        if(playerId != Globals.PersonalID)
-        {
-            new Bullet(this, playerId, x, y, direction);
-		}
-    }
+	private void InitializeWeapon()
+	{
+		Weapon initialWeapon = new Pistol();
+		_shootingHandler.SetWeapon(initialWeapon);
+	}
 
-    private void KeyIsDown(object sender, KeyEventArgs e)
-    {
-	    if (e.KeyCode == Keys.Space)
-	    {
-		    new Bullet(this, Globals.ThisPlayer, UpdateHandler);
-            UpdateHandler.HandleBulletFired(Globals.PersonalID);
-	    }
-    }
+	private void OnReceiveGameUpdate(string PlayersJson, string EnemiesJson)
+	{
+		var players = JsonConvert.DeserializeObject<List<Player>>(PlayersJson);
+
+		Player? thisPlayer = players.Where(p => p.Id == Globals.PersonalID).FirstOrDefault();
+		if (thisPlayer != null)
+			Globals.ThisPlayer.UpdatePlayer(thisPlayer);
+
+		players.ForEach(Render);
+
+		var enemies = DeserializeEnemies.DeserializeEnemy(EnemiesJson);
+		enemies.ForEach(Render);
+	}
+
+	private void Render(IRenderable renderable)
+	{
+		PictureBox? existingPicture = null;
+		foreach (var picture in this.Controls.OfType<PictureBox>())
+		{
+			if (picture.Tag is int numericTag && numericTag == renderable.Id)
+			{
+				existingPicture = picture;
+			}
+		}
+
+		if (existingPicture == null)
+		{
+			existingPicture = new();
+
+			Update(existingPicture, renderable);
+			this.Controls.Add(existingPicture);
+		}
+		else
+		{
+			Update(existingPicture, renderable);
+		}
+	}
+
+	private void Update(PictureBox picture, IRenderable renderable)
+	{
+		picture.Tag = renderable.Id;
+		picture.Left = renderable.X;
+		picture.Top = renderable.Y;
+		picture.SizeMode = PictureBoxSizeMode.AutoSize;
+		picture.Image = (Bitmap)Sprites.ResourceManager.GetObject(renderable.Image);
+	}
+
+	public bool PreFilterMessage(ref Message m)
+	{
+		UpdateHandler?.OnWindowsMessage(ref m);
+		return false;
+	}
+
+	private void label1_Click(object sender, EventArgs e) { }
+
+	private void KeyIsDown(object sender, KeyEventArgs e)
+	{
+		if (e.KeyCode == Keys.Space)
+		{
+			new Bullet(this, Globals.ThisPlayer, UpdateHandler);
+			UpdateHandler.HandleBulletFired(Globals.PersonalID);
+		}
+	}
 
 	/// <summary>
 	/// Repeats every set GameTimer interval (the interval is set from the Form1 design screen)
 	/// Mainly used for sending updates to the server about player actions
 	/// </summary>
 	private void GameTimer_Tick(object sender, EventArgs e) =>
-        _ = UpdateHandler?.UpdateAsync();
+		_ = UpdateHandler?.UpdateAsync();
 }
---
+++ Client/Mediator/Mediator.cs
@@ -0,0 +1,74 @@
+﻿using Client.HubClients;
+
+namespace Client.Mediator;
+
+public abstract class BaseComponent
+{
+	public GameMediator Mediator { get; set; }
+}
+
+public class GameMediator
+{
+	private readonly AssetManager _assetManager;
+	private readonly NetworkComponent _networkComponent;
+
+	public GameMediator(AssetManager assetManager, NetworkComponent networkComponent)
+	{
+		_assetManager = assetManager;
+		_networkComponent = networkComponent;
+
+		_assetManager.Mediator = this;
+		_networkComponent.Mediator = this;
+	}
+
+	public void Notify(object sender, string @event, object state)
+	{
+		if (@event == "RemoveEnemy")
+		{
+			_assetManager.RemoveEntity((int)state);
+		}
+		else if (@event == "SpawnBullet")
+		{
+			var stateArr = (object[])state;
+
+			var playerId = (int)stateArr[0];
+			var x = (int)stateArr[1];
+			var y = (int)stateArr[2];
+			var dir = (string)stateArr[3];
+			_assetManager.SpawnBullet(playerId, x, y, dir);
+		}
+	}
+}
+
+public class AssetManager : BaseComponent
+{
+	public void RemoveEntity(int id)
+	{
+		foreach (var picture in Globals.Form.Controls.OfType<PictureBox>())
+		{
+			if (picture.Tag is int numericTag && numericTag == id)
+			{
+				picture.Dispose();
+			}
+		}
+	}
+
+	public void SpawnBullet(int playerId, int x, int y, string direction)
+	{
+		if (playerId != Globals.PersonalID)
+		{
+			_ = new Bullet(Globals.Form, playerId, x, y, direction);
+		}
+	}
+}
+
+public class NetworkComponent : BaseComponent
+{
+	private readonly IHubClient? _hubClient;
+
+	public void OnRemoveEnemy(int id)
+		=> Mediator!.Notify(this, "RemoveEnemy", id);
+
+	public void OnSpawnBullet(int playerId, int x, int y, string direction)
+		=> Mediator!.Notify(this, "SpawnBullet", new object[] { playerId, x, y, direction });
+}
